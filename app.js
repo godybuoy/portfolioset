4e36765b8fbee8baebd39e851b703c44cfd61639
New Username <godybuoy@gmail.com>
2023-04-18 23:33:06 +0300

    Add popup javascript function

--- app.js
+++ app.js
@@ -37,3 +37,107 @@ const links = `
 const ul = document.createElement('ul');
 ul.innerHTML = links;
 desktop.appendChild(ul);
+
+const storeData = [
+
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (1).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (2).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Multi-post',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (3).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: './image/Snapshoot Portfolio (4).svg',
+    technologies: ['html', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+];
+
+const description = document.querySelectorAll('.work-paragraph');
+for (let i = 0; i < description.length; i += 2) {
+  description[i].innerHTML = storeData[i].description;
+  description[i + 1].innerHTML = storeData[i + 1].description;
+}
+
+// ..............popups...................
+
+const popups = document.querySelector('.popups');
+const popupProjectTitle = document.querySelector('.popup-project-title');
+const popupProjectDescription = document.querySelector('description');
+
+const disappearButton = document.querySelector('.popups-project-disapper-button');
+
+disappearButton.addEventListener('click', () => {
+  popups.style.display = 'none';
+});
+
+const popupsBootstrapLanguageDiv = document.querySelector('.bootstrap-language');
+const popupsRubyLanguageDiv = document.querySelector('.ruby-language');
+const popupsProjectDesktop = document.querySelector('.popups-project-p-desktop');
+const popupsProjectMobile = document.querySelector('.popups-project-p-mobile');
+const seeliveaTag = document.querySelector('.see-live-aTag');
+
+
+const btnOne = document.querySelector('.btn-one');
+
+btnOne.addEventListener('click', () => {
+  popups.style.display = 'flex';
+  document.querySelector('#popups-project-img').src = storeData[0].imagesrc;
+  popupProjectTitle.innerHTML = storeData[0].name;
+  popupsProjectDesktop.innerHTML = storeData[0].aboutProjectDesktop;
+  popupsProjectMobile.innerHTML = storeData[0].aboutProjectMobile;
+  seeliveaTag.href = storeData[0].href;
+  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
+});
+
+const btnTwo = document.querySelector('.btn-two');
+
+btnTwo.addEventListener('click', () => {
+  popups.style.display = 'flex';
+  document.querySelector('#popups-project-img').src = storeData[1].imagesrc;
+  popupProjectTitle.innerHTML = storeData[1].name;
+  popupsProjectDesktop.innerHTML = storeData[1].aboutProjectDesktop;
+  popupsProjectMobile.innerHTML = storeData[1].aboutProjectMobile;
+  seeliveaTag.href = storeData[1].href;
+  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
+});
+
+const btnThree = document.querySelector('.btn-three');
+
+btnThree.addEventListener('click', () => {
+  popups.style.display = 'flex';
+  document.querySelector('#popups-project-img').src = storeData[2].imagesrc;
+  popupProjectTitle.innerHTML = storeData[2].name;
+  popupProjectDescription.innerHTML = storeData[2].description;
+  popupsBootstrapLanguageDiv.classList.add('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.add('ruby-language-disappear');
+});
+
+const btnFour = document.querySelector('.btn-four');
+
+btnFour.addEventListener('click', () => {
+  popups.style.display = 'flex';
+  document.querySelector('#popups-project-img').src = storeData[3].imagesrc;
+  popupProjectTitle.innerHTML = storeData[3].name;
+  popupsBootstrapLanguageDiv.classList.remove('bootstrap-language-disappear');
+  popupsRubyLanguageDiv.classList.remove('ruby-language-disappear');
+});
