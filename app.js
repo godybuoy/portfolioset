c2c1fff435734bb5c95df655290e97579427ac9d
New Username <godybuoy@gmail.com>
2023-04-18 19:54:40 +0300

    Fix app linter

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const dh = document.getElementById("dh");
-const hamb = document.getElementById("hamb");
-const close = document.getElementById("close");
-const portfolio = document.getElementById("portfolio");
-const about = document.getElementById("about");
-const contact = document.getElementById("contact");
+const dh = document.getElementById('dh');
+const hamb = document.getElementById('hamb');
+const close = document.getElementById('close');
+const portfolio = document.getElementById('portfolio');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
 
-hamb.addEventListener("click", () => {
-  dh.classList.toggle("open");
+hamb.addEventListener('click', () => {
+  dh.classList.toggle('open');
 });
-portfolio.addEventListener("click", () => {
-  dh.classList.toggle("open");
+portfolio.addEventListener('click', () => {
+  dh.classList.toggle('open');
 });
-about.addEventListener("click", () => {
-  dh.classList.toggle("open");
+about.addEventListener('click', () => {
+  dh.classList.toggle('open');
 });
-contact.addEventListener("click", () => {
-  dh.classList.toggle("open");
+contact.addEventListener('click', () => {
+  dh.classList.toggle('open');
 });
-close.addEventListener("click", () => {
-  dh.classList.toggle("open");
+close.addEventListener('click', () => {
+  dh.classList.toggle('open');
 });
 
-const desktop = document.querySelector(".desktop-menu");
+const desktop = document.querySelector('.desktop-menu');
 const links = `
         <li id="portfolio">
           <a href="#Portfolio">Portfolio</a>
@@ -34,6 +34,6 @@ const links = `
         </li>
         <li class="close" id="close">&times;</li>
 `;
-const ul = document.createElement("ul");
+const ul = document.createElement('ul');
 ul.innerHTML = links;
 desktop.appendChild(ul);
