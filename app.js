6187e1caf5f9ff6244530c09457e261adc9e4f4b
juandapps <juandapps@hotmail.com>
2023-04-18 10:35:00 -0500

    Test fix desktop version navbar

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const dh = document.getElementById('dh');
-const hamb = document.getElementById('hamb');
-const close = document.getElementById('close');
-const portfolio = document.getElementById('portfolio');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
+const dh = document.getElementById("dh");
+const hamb = document.getElementById("hamb");
+const close = document.getElementById("close");
+const portfolio = document.getElementById("portfolio");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
+const screen = window.matchMedia("(min-width: 768px)");
 
-close.addEventListener('click', () => {
-  dh.classList.toggle('open');
-});
+function updateScreen() {
+  if (screen.matches) {
+    dh.style.display = "none";
+  }
+  console.log("Updating screen!!");
+}
 
-hamb.addEventListener('click', () => {
-  dh.classList.toggle('open');
+screen.addEventListener("change", updateScreen);
+
+hamb.addEventListener("click", () => {
+  dh.classList.toggle("open");
 });
-portfolio.addEventListener('click', () => {
-  dh.classList.toggle('open');
+portfolio.addEventListener("click", () => {
+  dh.classList.toggle("open");
 });
-about.addEventListener('click', () => {
-  dh.classList.toggle('open');
+about.addEventListener("click", () => {
+  dh.classList.toggle("open");
 });
-contact.addEventListener('click', () => {
-  dh.classList.toggle('open');
+contact.addEventListener("click", () => {
+  dh.classList.toggle("open");
 });
