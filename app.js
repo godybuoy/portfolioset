75a4fa12ae525028fa131bfc3b6705d13111e79a
New Username <godybuoy@gmail.com>
2023-04-21 23:42:30 +0300

    Add data store code

--- app.js
+++ app.js
@@ -177,3 +177,23 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
   }
 });
+
+const username = document.querySelector('#name');
+const useremail = document.querySelector('#email');
+const text = document.querySelector('#textarea');
+
+form.addEventListener('input', () => {
+  const formdata = {
+    username: username.value,
+    useremail: useremail.value,
+    text: text.value,
+  };
+
+  localStorage.setItem('data', JSON.stringify(formdata));
+});
+
+const formdataparsed = JSON.parse(localStorage.getItem('data'));
+
+username.value = formdataparsed.username;
+useremail.value = formdataparsed.useremail;
+text.value = formdataparsed.text;
